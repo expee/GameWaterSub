343407d05a36b6ba49a23cd7a814868fd7b2929d
expee <experian.elitiawan@gmail.com>
2017-04-25 00:13:50 +0700

    Add Obstacles Add player movement Enabled Ads, Analytics, and IAP

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float bodySpeed;
+    public float tilt;
+	private Rigidbody rb;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+        rb = gameObject.GetComponent<Rigidbody>();
 	}
-	
+
+	void FixedUpdate()
+	{
+        float moveVer = Input.GetAxis("Vertical");
+        float moveHor = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHor, moveVer, 0.0f);
+        rb.velocity = movement * bodySpeed;
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.y * tilt);
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 }
